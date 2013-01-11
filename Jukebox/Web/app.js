cbff0eac250e4e52476ea0601ff57095a00a7d19
Kasra Kyanzadeh <me@kasrak.com>
2013-01-10 19:21:13 -0500

    Library refactor

--- Jukebox/Web/app.js
+++ Jukebox/Web/app.js
@@ -1,8 +1,98 @@
-"use strict";
-
-var $library, library; // xxx 
+'use strict';
 
 var server = '';
+
+var Library = (function() {
+    var library = {},
+        _artists = null,
+        _songs = null;
+
+
+    function load(success) {
+        $.getJSON(server + '/songs', function(data, status) {
+            if (data.error && data.error == 'not ready') {
+                setTimeout(load, 500);
+            } else {
+                library = data;
+
+                // Destroy caches
+                _artists = null;
+                _songs = null;
+
+                success();
+            }
+        });
+    }
+
+    function isEmpty() {
+        return artists().length === 0;
+    }
+
+    function artists() {
+        if (!_artists) {
+            _artists = Object.keys(library).sort(stringCompare);
+        }
+
+        return _artists;
+    }
+
+    function albums(artist) {
+        return (library[artist]) ? Object.keys(library[artist]).sort(stringCompare) : [];
+    }
+
+    function songs(artist, album) {
+        if (!artist) {
+            // all songs
+            if (!_songs) {
+                _songs = [];
+                _.each(artists(), function(artist) {
+                    _songs = _songs.concat(songs(artist));
+                });
+
+                _songs = _songs.sort(songCompare);
+            }
+
+            return _songs;
+        } else if (!album) {
+            // all of artist's songs
+            var artistSongs = [];
+            _.each(library[artist], function(albumSongs) {
+                artistSongs = artistSongs.concat(albumSongs);
+            });
+            return artistSongs;
+        } else {
+            return (library[artist]) ? library[artist][album].sort(songCompare) : [];
+        }
+    }
+
+    function songCompare(a, b) {
+        return stringCompare(a[0], b[0]);
+    }
+
+    function stringCompare(a, b) {
+        a = a.trim().toLowerCase();
+        b = b.trim().toLowerCase();
+
+        if (a.slice(0, 4) == "the ") a = a.slice(4);
+        if (b.slice(0, 4) == "the ") b = b.slice(4);
+
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    return {
+        load: load,
+        isEmpty: isEmpty,
+        artists: artists,
+        albums: albums,
+        songs: songs
+    };
+}());
 
 var Status = {
     data: {},
@@ -17,7 +107,7 @@ var Status = {
 
             if (self.isUpdating) {
                 setTimeout(function() {
-                    Status.update();
+                    self.update();
                 }, 1500);
             }
         });
@@ -61,61 +151,42 @@ var Status = {
     }
 };
 
-function getSongs() {
-    $.getJSON(server + '/songs', function(data, status) {
-        if (status == 'success') {
-            if (data.error && data.error == 'not ready') {
-                setTimeout(getSongs, 500);
-            } else {
-                library = data;
-                renderLibrary();
-            }
-        } else {
-            setTimeout(getSongs, 1000);
-        }
-    });
-}
-
-function renderLibrary() {
-    if (library === null) {
-        $library.html('No songs to display');
+function renderArtistsView(library, container) {
+    if (library.isEmpty()) {
+        container.html('No songs to display');
         return;
     }
 
-    $library.html('');
-    var artists = Object.keys(library).sort();
+    container.html('');
 
-    for (var i in artists) {
-        var artist = artists[i],
-            albums = Object.keys(library[artist]).sort(),
-            $artist = $('<div class="artist">').html('<br><a class="artist">' + artist + '</a>'),
+    _.each(library.artists(), function(artist) {
+        var $artist = $('<div class="artist">').html('<a class="artist">' + artist + '</a>'),
             $albums = $('<div class="albums">');
         
-        for (var j in albums) {
-            var album = albums[j],
-                songs = library[artist][album];
-
+        _.each(library.albums(artist), function(album) {
             $albums.append('<h3>' + album + '</h3>');
             
-            for (var k in songs) {
-                var song = songs[k];
-                $albums.append('<a class="song" data-id="' + song[1] + '">' + song[0] + '</a><br>');
-            }
-        }
+            _.each(library.songs(artist, album), function(song) {
+                $albums.append('<a class="song" data-id="' + song[1] + '">' + song[0] + '</a>');
+            });
+        });
 
         $artist.append($albums);
-        $library.append($artist);
-    }
+        container.append($artist);
+    });
+
     $('div.albums').hide();
 }
 
 $(function() {
-    $library = $('#library');
-    var $np = $('#now_playing'),
+    var $library = $('#library'),
+        $np = $('#now_playing'),
         $volume = $('#volume'),
         $play = $('#play');
 
-    getSongs();
+    Library.load(function() {
+        renderArtistsView(Library, $library);
+    });
 
     Status.on('artist', function(key, value) {
         $np.find('.artist').html(value);
